205d0216a71631aeb088b3dae1eb57b5d020f4a2
bastien707 <bastien.facqueur@efrei.net>
2021-04-05 16:55:48 +0200

    initialized

---
+++ src/StatePackage/State.java
@@ -0,0 +1,7 @@
+package StatePackage;
+
+public class State {
+    private int index; // the reference of the state
+    private boolean isTerminal; // 0  = not terminal, 1  = terminal
+    private boolean isInitial; // 0  = not initial, 1  = initial
+}
---
+++ src/TransitionPackage/Transition.java
@@ -0,0 +1,9 @@
+package TransitionPackage;
+
+import StatePackage.*;
+
+public class Transition {
+    private State iState; //initial State
+    private State fState; //final State
+    char letter; //transition letter
+}
