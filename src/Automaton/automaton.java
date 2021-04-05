190d179d6fcff6f48948296803a058a79e0a1b00
bastien707 <bastien.facqueur@efrei.net>
2021-04-05 16:55:05 +0200

    constructor added

--- src/Automaton/automaton.java
+++ src/Automaton/automaton.java
@@ -1,5 +1,33 @@
 package Automaton;
 
+import org.w3c.dom.Text;
+
+import StatePackage.*;
+import TransitionPackage.*;
+
 public class automaton {
+    private int symbol; // nbr of symbols, e.g if equal 3 -> a,b,c
+    private int stateNumber; // number of States
+    private State iStateArray[]; // Array of Initial State(s)
+    private State fStateArray[]; // Array of Final State(s)
+    private int transitionNumber; // Number of transitions
+    private Transition transitionArray[]; // Array of transitions
+
+    public automaton(final int symbol, final int stateNumber, final State iStateArray[], final State fStateArray[],
+            final int transitionNumber, final Transition transitionArray[]) 
+    {
+        this.symbol = symbol;
+        this.stateNumber = stateNumber;
+        this.iStateArray = iStateArray;
+        this.fStateArray = fStateArray;
+        this.transitionNumber = transitionNumber;
+        this.transitionArray = transitionArray;
+    }
     
+    public int recoverSymbol(){ //return number of symbol from text file
+        return 0;
+    }
+    public static void main(String argvs[]){
+        System.out.println("test");
+    }
 }
