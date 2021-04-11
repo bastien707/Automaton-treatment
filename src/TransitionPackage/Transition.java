1b30582e91339760647e7d6d0aaf517f4c740ddf
bastien707 <bastien.facqueur@efrei.net>
2021-04-11 16:25:43 +0200

    getters added

--- src/TransitionPackage/Transition.java
+++ src/TransitionPackage/Transition.java
@@ -3,7 +3,42 @@ package TransitionPackage;
 import StatePackage.*;
 
 public class Transition {
-    private State iState; //initial State
-    private State fState; //final State
+    private State EntryState; //initial State
+    private State ArrivalState; //final State
     char letter; //transition letter
+
+    public Transition(final State EntryState, final char letter, final State ArrivalState){
+        this.EntryState = EntryState;
+        this.letter = letter;
+        this.ArrivalState = ArrivalState;
+    }
+
+    //getters
+
+    public State getEntryState() {
+        return EntryState;
+    }
+
+    public State getArrivalState() {
+        return ArrivalState;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+
+    @Override
+    public String toString() {
+        return EntryState.getIndex() +""+""+ letter +""+""+ArrivalState.getIndex();
+    }
+
+    public static void main(String[] args) {
+        State Es = new State(0,true,false);
+        State Fs = new State(1,false,true);
+        Transition t1 = new Transition(Es,'a',Fs);
+        System.out.println(t1);
+    }
 }
+
+
